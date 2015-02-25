4d2b6377a907dbcbfa5d8666e9ee49393ac670e0
Üsame Fethullah AVCI <takaharashujin@gmail.com>
2015-02-25 02:03:21 +0200

    Name Change

--- SesliKomut1/Form1.Designer.cs
+++ SesliKomut1/Form1.Designer.cs
@@ -109,7 +109,7 @@
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.btnBrowse);
             this.Name = "Form1";
-            this.Text = "Komut Sistemi";
+            this.Text = "Merep";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
